f59ea68ff15db41902c2379e14c8ec9ffbeb6252
wv-tud <w.vlenterie@student.tudelft.nl>
2016-09-01 12:57:51 +0200

    Added ivybus reference example for piksi GPS

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 [RTCM 10403.2 reference:           https://www.u-blox.com/sites/default/files/NEO-M8P_DataSheet_(UBX-15016656).pdf]
 [RTCM Structure reference:         https://github.com/wv-tud/ubx_RTK_groundstation/blob/master/087_rubinov_collier_fuller_seager.pdf]
 [RTCM message content reference:   https://github.com/tomojitakasu/RTKLIB/blob/master/src/rtcm3.c]
+[Ivybus reference:                 https://github.com/paparazzi/paparazzi/blob/a99dfbd5e8ab94f6e227b0e8daca1cf7677b088e/sw/ground_segment/misc/sbp2ivy.c]
 */
 
 #define READ_PREAMBLE 0
