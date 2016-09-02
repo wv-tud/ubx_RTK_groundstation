69be0798573148cbfec58df2cccdfb259786b010
wv-tud <w.vlenterie@student.tudelft.nl>
2016-09-02 11:17:46 +0200

    Fixed ecef to lla pos.

--- main.c
+++ main.c
@@ -19,6 +19,7 @@
 #define GPS_PORT "/dev/ttyUSB0"
 #endif
 
+#include <math.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
@@ -26,6 +27,218 @@
 #include <string.h>
 #include <termios.h>
 #include <unistd.h>
+
+struct EcefCoor_f {
+  float x; ///< in meters
+  float y; ///< in meters
+  float z; ///< in meters
+};
+
+struct LlaCoor_f {
+  float lat; ///< in radians
+  float lon; ///< in radians
+  float alt; ///< in meters (normally above WGS84 reference ellipsoid)
+};
+
+struct LlaCoor_f lla_of_ecef_f(struct EcefCoor_f posecefpos);
+int set_interface_attribs(int fd, int speed);
+void set_mincount(int fd, int mcount);
+unsigned int getbitu(unsigned char *buff, int pos, int len);
+unsigned int getbitu(unsigned char *buff, int pos, int len);
+int getbits(unsigned char *buff, int pos, int len);
+static double getbits_38(unsigned char *buff, int pos);
+
+#define MSG_DEST	"ground"
+#define MSG_NAME    "FLIGHT_PARAM"
+#define MSG_ID		"GCS"
+
+int main()
+{
+	// Initialize variables
+	int fd;
+	int len = 0;
+	int len1 = 0;
+	int rawIndex = 0;
+	int checksumCounter = 0;
+	int byteIndex = 0;
+	unsigned char raw_msg[1024], message[100];
+	// Set GPS port
+	char *portname = GPS_PORT;
+	//char* ivy_bus;
+	//ivy_bus = "127.255.255.255";
+	// Open serial port
+	fd = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
+	if (fd < 0) {
+		printf("Error opening %s: %s\n", portname, strerror(errno));
+		return -1;
+	}
+	/*baudrate 9600, 8 bits, no parity, 1 stop bit */
+	set_interface_attribs(fd, B9600);
+	//set_mincount(fd, 0);
+	/* set to pure timed read */
+
+	/* simple output */
+	/*
+    int wlen = write(fd, "Hello!\n", 7);
+    if (wlen != 7) {
+        printf("Error from write: %d, %d\n", wlen, errno);
+    }
+    tcdrain(fd); delay for output
+	 */
+	//IvyInit ("GPSd2Ivy", "GPSd2Ivy READY", NULL, NULL, NULL, NULL);
+	//IvyStart(ivy_bus);
+
+	/* simple noncanonical input */
+	int status = 0;
+	do {
+		unsigned char buf[3];
+		int rdlen;
+
+		rdlen = read(fd, buf, sizeof(buf) - 1);
+		if (rdlen > 0) {
+			switch (status){
+			case READ_PREAMBLE:
+				if (((int) buf[0]) == 211){
+					status = READ_RESERVED;
+					rawIndex = 0;
+					raw_msg[rawIndex] = buf[0];
+					checksumCounter = 0;
+					byteIndex = 0;
+				}
+				break;
+			case READ_RESERVED:
+				raw_msg[rawIndex] = buf[0];
+				len1 = ((int) buf[0])  & 0b00000011;
+				status = READ_LENGTH;
+				break;
+			case READ_LENGTH:
+				raw_msg[rawIndex] = buf[0];
+				len = (len1 << 8) + ((int) buf[0]) ;
+				if(len==19) printf("\n");
+				status = READ_MESSAGE;
+				break;
+			case READ_MESSAGE:
+				raw_msg[rawIndex] = buf[0];
+				// read
+				message[byteIndex] = buf[0];
+				if (byteIndex == (len - 1))
+				{
+					status = READ_CHECKSUM;
+
+				}
+				byteIndex++;
+				break;
+			case READ_CHECKSUM:
+				raw_msg[rawIndex] = buf[0];
+				checksumCounter++;
+				if(checksumCounter == 3)
+				{
+					// Check the message
+					status = READ_PREAMBLE;
+					// We've checked the message and it checks out
+					// Check what message type it is
+					unsigned char *b = message;
+					int msg1 = *b;
+					b++;
+					int msg2 = *b;
+					int RTK_msgtype = (msg1 << 4) + (msg2 >> 4);
+					printf ("RTKmsgType: %i\n", RTK_msgtype);
+
+					// Printing useful messages from RTK1005 msg.
+					if (RTK_msgtype == 1005){
+						int i = 1;
+						int MsgId 		= getbitu(message, 0, 12);
+						int StaId 		= getbitu(message, 12, 12);
+						int ItRef 		= getbitu(message, 24, 6);
+						int indGPS 		= getbitu(message, 30, 1);
+						int indGlonass 	= getbitu(message, 31, 1);
+						int indGalileo 	= getbitu(message, 32, 1);
+						int indRefS 	= getbitu(message, 33, 1);
+						struct EcefCoor_f posecef;
+						posecef.x = getbits_38(message,34) * 0.0001;
+						posecef.y = getbits_38(message,74) * 0.0001;
+						posecef.z = getbits_38(message,114) * 0.0001;
+						struct LlaCoor_f poslla;
+						poslla = lla_of_ecef_f(posecef);
+						printf("To send: (Lat: %f) \t (Lon: %f) \t (Alt: %f)\n", poslla.lat / (2*3.1415) * 360, poslla.lon / (2*3.1415) * 360, poslla.alt);
+						printf("MsgId: %i \n", MsgId); printf("StaId: %i \n", StaId); printf("ItRef: %i \n", ItRef);
+						printf("indGPS: %i \n", indGPS);
+						printf("indGlonass: %i \n", indGlonass);
+						printf("indGallileo: %i \n", indGalileo);
+						printf("indRefS: %i \n", indGalileo);
+
+						/*
+						 IvySendMsg("%s %s %s %f %f %f %f %f %f %f %f %f %f %f %d %f",
+						                 MSG_DEST,
+						                 MSG_NAME,
+						                 MSG_ID, // ac_id
+						                 0.0, // roll,
+						                 0.0, // pitch,
+						                 0.0, // heading
+						 				llh[0],
+						 				llh[1],
+						                 0,
+						                 0, // course
+						                 llh[2],
+						                 0,
+						                 0.0, // agl
+						                 0,
+						                 0, // itow
+						                 0.0); // airspeed
+*/
+					}
+					else{
+						// Messages 1077 1087
+					}
+					printf("\n");
+				}
+				break;
+			}
+			rawIndex++;
+		} else if (rdlen < 0) {
+			printf("Error from read: %d: %s\n", rdlen, strerror(errno));
+		}
+		/* repeat read to get full message */
+	} while (1);
+}
+
+struct LlaCoor_f lla_of_ecef_f(struct EcefCoor_f posecefpos)
+{
+	struct LlaCoor_f posllapos;
+	// FIXME : make an ellipsoid struct
+  static const float a = 6378137.0;           /* earth semimajor axis in meters */
+  static const float f = 1. / 298.257223563;  /* reciprocal flattening          */
+  const float b = a * (1. - f);               /* semi-minor axis                */
+  const float b2 = b * b;
+
+  const float e2 = 2.*f - (f * f);            /* first eccentricity squared     */
+  const float ep2 = f * (2. - f) / ((1. - f) * (1. - f)); /* second eccentricity squared    */
+  const float E2 = a * a - b2;
+
+
+  const float z2 = posecefpos.z * posecefpos.z;
+  const float r2 = posecefpos.x * posecefpos.x + posecefpos.y * posecefpos.y;
+  const float r = sqrtf(r2);
+  const float F = 54.*b2 * z2;
+  const float G = r2 + (1 - e2) * z2 - e2 * E2;
+  const float c = (e2 * e2 * F * r2) / (G * G * G);
+  const float s = powf((1 + c + sqrtf(c * c + 2 * c)), 1. / 3.);
+  const float s1 = 1 + s + 1 / s;
+  const float P = F / (3 * s1 * s1 * G * G);
+  const float Q = sqrtf(1 + 2 * e2 * e2 * P);
+  const float ro = -(e2 * P * r) / (1 + Q) + sqrtf((a * a / 2) * (1 + 1 / Q) - ((1 - e2) * P * z2) / (Q *
+                   (1 + Q)) - P * r2 / 2);
+  const float tmp = (r - e2 * ro) * (r - e2 * ro);
+  const float U = sqrtf(tmp + z2);
+  const float V = sqrtf(tmp + (1 - e2) * z2);
+  const float zo = (b2 * posecefpos.z) / (a * V);
+
+  posllapos.alt = U * (1 - b2 / (a * V));
+  posllapos.lat = atanf((posecefpos.z + ep2 * zo) / r);
+  posllapos.lon = atan2f(posecefpos.y, posecefpos.x);
+
+  return posllapos;
+}
 
 int set_interface_attribs(int fd, int speed)
 {
@@ -77,215 +290,22 @@ void set_mincount(int fd, int mcount)
 		printf("Error tcsetattr: %s\n", strerror(errno));
 }
 
-int main()
+unsigned int getbitu(unsigned char *buff, int pos, int len)
 {
-	// Initialize variables
-	int fd;
-	int len = 0;
-	int len1 = 0;
-	int rawCounter = 0;
-	int checksumCounter = 0;
-	int byteCounter = 0;
-	unsigned char raw_msg[1024], message[100];
-	// Set GPS port
-	char *portname = GPS_PORT;
-	// Open serial port
-	fd = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
-	if (fd < 0) {
-		printf("Error opening %s: %s\n", portname, strerror(errno));
-		return -1;
-	}
-	/*baudrate 9600, 8 bits, no parity, 1 stop bit */
-	set_interface_attribs(fd, B9600);
-	//set_mincount(fd, 0);
-	/* set to pure timed read */
+    unsigned int bits=0;
+    int i;
+    for (i=pos;i<pos+len;i++) bits=(bits<<1)+((buff[i/8]>>(7-i%8))&1u);
+    return bits;
+}
 
-	/* simple output */
-	/*
-    int wlen = write(fd, "Hello!\n", 7);
-    if (wlen != 7) {
-        printf("Error from write: %d, %d\n", wlen, errno);
-    }
-    tcdrain(fd); delay for output
-	 */
+int getbits(unsigned char *buff, int pos, int len)
+{
+    unsigned int bits=getbitu(buff,pos,len);
+    if (len<=0||32<=len||!(bits&(1u<<(len-1)))) return (int)bits;
+    return (int)(bits|(~0u<<len)); /* extend sign */
+}
 
-
-	/* simple noncanonical input */
-	int status = 0;
-	do {
-		unsigned char buf[3];
-		int rdlen;
-
-		rdlen = read(fd, buf, sizeof(buf) - 1);
-		if (rdlen > 0) {
-			switch (status){
-			case READ_PREAMBLE:
-				if (((int) buf[0]) == 211){
-					status = READ_RESERVED;
-					rawCounter = 0;
-					raw_msg[rawCounter] = buf[0];
-					checksumCounter = 0;
-					byteCounter = 0;
-				}
-				break;
-			case READ_RESERVED:
-				raw_msg[rawCounter] = buf[0];
-				len1 = ((int) buf[0])  & 0x00000011;
-				status = READ_LENGTH;
-				break;
-			case READ_LENGTH:
-				raw_msg[rawCounter] = buf[0];
-				len = (len1 << 8) + ((int) buf[0]) ;
-				if(len==19) printf("\n");
-				status = READ_MESSAGE;
-				break;
-			case READ_MESSAGE:
-				raw_msg[rawCounter] = buf[0];
-				// read
-				message[byteCounter] = buf[0];
-				if (byteCounter == (len - 1))
-				{
-					status = READ_CHECKSUM;
-
-				}
-				byteCounter++;
-				break;
-			case READ_CHECKSUM:
-				raw_msg[rawCounter] = buf[0];
-				checksumCounter++;
-				if(checksumCounter == 3)
-				{
-					// Check the message
-					status = READ_PREAMBLE;
-					// We've checked the message and it checks out
-					// Check what message type it is
-					unsigned char *b = message;
-					int msg1 = *b;
-					b++;
-					int msg2 = *b;
-					int RTK_msgtype = (msg1 << 4) + (msg2 >> 4);
-					printf ("RTKmsgType: %i\n", RTK_msgtype);
-
-					// Printing useful messages from RTK1005 msg.
-					unsigned char *p;
-					if (RTK_msgtype == 1005){
-						p = message;
-						int MsgId = 0;
-						int StaId = 0;
-						int ItRef = 0;
-						int indGPS;
-						int indGlonass;
-						int indGalileo;
-						int indRefS;
-						unsigned char Xpos[5];
-						double xPos;
-						unsigned char Ypos[5];
-						unsigned char Zpos[5];
-						int i = 1;
-						for (i=1; i<=19; i++, p++){
-							switch (i){
-							case 1:{
-								MsgId = ((int) *p)<<4;
-								break;
-							}
-							case 2:{
-								MsgId += ((int) *p)>>4;
-								StaId =  ((int) *p & 0x00001111)<<8;
-								break;
-							}
-							case 3:{
-								StaId += ((int) *p);
-								break;
-							}
-							case 4:{
-								ItRef = ((int) *p)>>2;
-								indGPS     = ((int) *p & 0x00000010) >> 1;
-								indGlonass = (int) *p & 0x00000001;
-								break;
-							}
-							case 5: {
-								indGalileo = ((int) *p & 0x10000000) >> 7;
-								indRefS    = ((int) *p & 0x01000000) >> 6;
-								Xpos[0]        = *p & 0x00111111;
-								break;
-							}
-							case 6:
-								Xpos[1]        = *p;
-								break;
-							case 7:
-								Xpos[2]        = *p;
-								break;
-							case 8:
-								Xpos[3]        = *p;
-								break;
-							case 9:
-								Xpos[4]        = *p;
-								xPos           = *((double*) Xpos) * 0.0001;
-								break;
-							case 10:{
-								//Ypos = ((long long) (*p & 0x00111111))<<32;
-								break;
-							}
-							case 11:
-								//Ypos += ((long long)*p)<<24;
-								break;
-							case 12:
-								//Ypos += ((long long)*p)<<16;
-								break;
-							case 13:
-								//Ypos += ((long long)*p)<<8;
-								break;
-							case 14:
-								//Ypos += ((long long)*p);
-								//Ypos = Ypos*0.0001;
-								break;
-							case 15:{
-								//Zpos = ((long long) (*p & 0x00111111))<<32;
-								break;
-							}
-							case 16:
-								//Zpos += ((long long)*p)<<24;
-								break;
-							case 17:
-								//Zpos += ((long long)*p)<<16;
-								break;
-							case 18:
-								//Zpos += ((long long)*p)<<8;
-								break;
-							case 19:
-								//Zpos += ((long long)*p);
-								//Zpos = Zpos*0.0001;
-								break;
-							}
-						}
-						printf("MsgId: %i \n", MsgId); printf("StaId: %i \n", StaId); printf("ItRef: %i \n", ItRef);
-						printf("indGPS: %i \n", indGPS);
-						printf("indGlonass: %i \n", indGlonass);
-						printf("indGallileo: %i \n", indGalileo);
-						printf("indRefS: %i \n", indGalileo);
-						printf("Xpos: %0.0f \n", xPos);// printf("Ypos: %0.2f \n", (double) Ypos); printf("Zpos: %0.2f \n", (double) Zpos);
-					}
-					else{
-						for (p = message; byteCounter> 0;byteCounter--, p++){
-							//printf(" 0x%x", *p);
-						}
-					}
-					//printf("\nRAW:\n");
-					unsigned char* raw_ptr = raw_msg;
-					int i;
-					for(i = 0; i <= rawCounter; i++)
-					{
-						//printf(" 0x%x", *raw_ptr);
-						raw_ptr++;
-					}
-					printf("\n");
-				}
-				break;
-			}
-			rawCounter++;
-		} else if (rdlen < 0) {
-			printf("Error from read: %d: %s\n", rdlen, strerror(errno));
-		}
-		/* repeat read to get full message */
-	} while (1);
+static double getbits_38(unsigned char *buff, int pos)
+{
+    return (double)getbits(buff,pos,32)*64.0+getbitu(buff,pos+32,6);
 }
