286557623126a1c909a455869ec22b7b6559b265
wv-tud <w.vlenterie@student.tudelft.nl>
2016-09-02 16:15:56 +0200

    Code cleanup & tested example.c

--- example.c
+++ example.c
@@ -7,12 +7,13 @@
 [RTCM message content reference:   https://github.com/tomojitakasu/RTKLIB/blob/master/src/rtcm3.c]
 [Ivybus reference:                 https://github.com/paparazzi/paparazzi/blob/a99dfbd5e8ab94f6e227b0e8daca1cf7677b088e/sw/ground_segment/misc/sbp2ivy.c]
 [1005 reference:                   https://github.com/jcmb/RTCM3/blob/master/DEFS/1005.RTCM3]
-*/
-
+ */
+// Pre-processor definitions
 #ifndef GPS_PORT
 #define GPS_PORT "/dev/ttyUSB0"
 #endif
-
+#define NO_CALLBACK
+// Includes
 #include <math.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -23,19 +24,17 @@
 #include <unistd.h>
 #include <stdbool.h>
 #include "rtcm3.h"
-
+// Structure definitions
 struct EcefCoor_f {
-  float x; ///< in meters
-  float y; ///< in meters
-  float z; ///< in meters
+	float x; ///< in meters
+	float y; ///< in meters
+	float z; ///< in meters
 };
-
 struct LlaCoor_f {
-  float lat; ///< in radians
-  float lon; ///< in radians
-  float alt; ///< in meters (normally above WGS84 reference ellipsoid)
+	float lat; ///< in radians
+	float lon; ///< in radians
+	float alt; ///< in meters (normally above WGS84 reference ellipsoid)
 };
-
 struct RTCM3_1005 {
 	int StaId;
 	int ItRef;
@@ -46,47 +45,24 @@ struct RTCM3_1005 {
 	struct EcefCoor_f posEcef;
 	struct LlaCoor_f posLla;
 };
-
 struct RTCM3_msg {
 	int length;
 	int type;
 	unsigned char raw_msg[1024];
 	struct RTCM3_1005 *msg1005;
 };
-
+// Function declarations
 struct LlaCoor_f lla_of_ecef_f(struct EcefCoor_f);
 int              set_interface_attribs(int, int);
 void             set_mincount(int, int);
-unsigned int     getbitu(unsigned char *, int , int );
-int              getbits(unsigned char *, int , int );
-static double    getbits_38(unsigned char *, int );
-void             readMessage(int *, struct RTCM3_msg *);
-
+static uint32_t rtcm3_read(uint8_t *, uint32_t n, void * __attribute__((unused)));
 // Required global variables
 int fd;
 
-#define MSG_DEST	"ground"
-#define MSG_NAME    "FLIGHT_PARAM"
-#define MSG_ID		"GCS"
-
-
-static uint32_t sbp_read(uint8_t *buff, uint32_t n, void *context __attribute__((unused)))
-{
-  int ret = read(fd, buff, n);
-  if(ret > 0)
-    return ret;
-  else
-    return 0;
-}
-
-
 int main()
 {
-	// Initialize variables
 	// Set GPS port
 	char *portname = GPS_PORT;
-	//char* ivy_bus;
-	//ivy_bus = "127.255.255.255";
 	// Open serial port
 	fd = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
 	if (fd < 0) {
@@ -97,71 +73,45 @@ int main()
 	set_interface_attribs(fd, B9600);
 	//set_mincount(fd, 0);
 	/* set to pure timed read */
-
-	/* simple output */
-	/*
-    int wlen = write(fd, "Hello!\n", 7);
-    if (wlen != 7) {
-        printf("Error from write: %d, %d\n", wlen, errno);
-    }
-    tcdrain(fd); delay for output
-	 */
-	//IvyInit ("GPSd2Ivy", "GPSd2Ivy READY", NULL, NULL, NULL, NULL);
-	//IvyStart(ivy_bus);
-
-	/* simple noncanonical input */
-	sbp_state_t state;
+	rtcm3_state_t state;
+	state.state  = READ_PREAMBLE;
+	state.n_read = 0;
 	int readstatus;
 	while ( 1 == 1 )
 	{
-		readstatus = sbp_process(&state, &sbp_read);
-		if (readstatus ==  SBP_OK_CALLBACK_EXECUTED){
-
-		printf("Read message %i of length %i\n", state.msg_type, state.msg_len);
-		if (state.msg_type == 1005){
-			struct RTCM3_1005 msg1005;
-
-			msg1005.StaId 		= getbitu(state.msg_buff, 24 + 12, 12);
-			msg1005.ItRef 		= getbitu(state.msg_buff, 24 + 24, 6);
-			msg1005.indGPS 		= getbitu(state.msg_buff, 24 + 30, 1);
-			msg1005.indGlonass 	= getbitu(state.msg_buff, 24 + 31, 1);
-			msg1005.indGalileo 	= getbitu(state.msg_buff, 24 + 32, 1);
-			msg1005.indRefS 	= getbitu(state.msg_buff, 24 + 33, 1);
-			msg1005.posEcef.x   = getbits_38(state.msg_buff, 24 + 34) * 0.0001;
-			msg1005.posEcef.y   = getbits_38(state.msg_buff, 24 + 74) * 0.0001;
-			msg1005.posEcef.z   = getbits_38(state.msg_buff, 24 + 114) * 0.0001;
-			msg1005.posLla      = lla_of_ecef_f(msg1005.posEcef);
-
-			printf("To send: (Lat: %f) \t (Lon: %f) \t (Alt: %f)\n", msg1005.posLla.lat / (2*3.1415) * 360, msg1005.posLla.lon / (2*3.1415) * 360, msg1005.posLla.alt);
-			printf("StaId: %i \n", msg1005.StaId);
-			printf("ItRef: %i \n", msg1005.ItRef);
-			printf("indGPS: %i \n", msg1005.indGPS);
-			printf("indGlonass: %i \n", msg1005.indGlonass);
-			printf("indGallileo: %i \n", msg1005.indGalileo);
-			printf("indRefS: %i \n", msg1005.indRefS);
-
+		readstatus = rtcm3_process(&state, &rtcm3_read);
+		if (readstatus ==  RTCM3_OK_CALLBACK_EXECUTED){
+			switch(state.msg_type){
+			case 1077:
+			case 1087:
+				printf("Read message %i of length %i\n", state.msg_type, state.msg_len);
+				break;
+			case 1005:
+				printf("Read message %i of length %i\n", state.msg_type, state.msg_len);
+				struct RTCM3_1005 msg1005;
+				msg1005.StaId 		= RTCMgetbitu(state.msg_buff, 24 + 12, 12);
+				msg1005.ItRef 		= RTCMgetbitu(state.msg_buff, 24 + 24, 6);
+				msg1005.indGPS 		= RTCMgetbitu(state.msg_buff, 24 + 30, 1);
+				msg1005.indGlonass 	= RTCMgetbitu(state.msg_buff, 24 + 31, 1);
+				msg1005.indGalileo 	= RTCMgetbitu(state.msg_buff, 24 + 32, 1);
+				msg1005.indRefS 	= RTCMgetbitu(state.msg_buff, 24 + 33, 1);
+				msg1005.posEcef.x   = RTCMgetbits_38(state.msg_buff, 24 + 34) * 0.0001;
+				msg1005.posEcef.y   = RTCMgetbits_38(state.msg_buff, 24 + 74) * 0.0001;
+				msg1005.posEcef.z   = RTCMgetbits_38(state.msg_buff, 24 + 114) * 0.0001;
+				msg1005.posLla      = lla_of_ecef_f(msg1005.posEcef);
+				printf("To send: (Lat: %f) \t (Lon: %f) \t (Alt: %f)\n", msg1005.posLla.lat / (2*3.1415) * 360, msg1005.posLla.lon / (2*3.1415) * 360, msg1005.posLla.alt);
+				printf("StaId: %i \n", msg1005.StaId);
+				printf("ItRef: %i \n", msg1005.ItRef);
+				printf("indGPS: %i \n", msg1005.indGPS);
+				printf("indGlonass: %i \n", msg1005.indGlonass);
+				printf("indGallileo: %i \n", msg1005.indGalileo);
+				printf("indRefS: %i \n", msg1005.indRefS);
+				break;
+			default :
+				printf("Erronous message type: %i\n", state.msg_type);
+			}
 		}
-			/*
-		IvySendMsg("%s %s %s %f %f %f %f %f %f %f %f %f %f %f %d %f",
-				MSG_DEST,
-				MSG_NAME,
-				MSG_ID, // ac_id
-				0.0, // roll,
-				0.0, // pitch,
-				0.0, // heading
-				llh[0],
-				llh[1],
-				0,
-				0, // course
-				llh[2],
-				0,
-				0.0, // agl
-				0,
-				0, // itow
-				0.0); // airspeed
-			 */
-		}
-		else if (readstatus==SBP_CRC_ERROR){
+		else if (readstatus==RTCM3_CRC_ERROR){
 			printf("Error: Resetting status to Preamble \n");
 			state.state = READ_PREAMBLE;
 		}
@@ -169,62 +119,14 @@ int main()
 	return 0;
 }
 
-//void  readMessage(int *fd, struct RTCM3_msg *message){
-//	int len = 0;
-//	int len1 = 0;
-//	do {
-//		unsigned char buf[3];
-//		int rdlen;
-//		rdlen = read(*fd, buf, sizeof(buf) - 1);
-//		if (rdlen > 0) {
-//			switch (status){
-//			case READ_PREAMBLE:
-//				if (((int) buf[0]) == 211){
-//					status = READ_RESERVED;
-//					rawIndex = 0;
-//					message->raw_msg[rawIndex] = buf[0];
-//					checksumCounter = 0;
-//					byteIndex = 0;
-//				}
-//				break;
-//			case READ_RESERVED:
-//				message->raw_msg[rawIndex] = buf[0];
-//				len1 = ((int) buf[0])  & 0b00000011;
-//				status = READ_LENGTH;
-//				break;
-//			case READ_LENGTH:
-//				message->raw_msg[rawIndex] = buf[0];
-//				len = (len1 << 8) + ((int) buf[0]) ;
-//				status = READ_MESSAGE;
-//				break;
-//			case READ_MESSAGE:
-//				message->raw_msg[rawIndex] = buf[0];
-//				if (byteIndex == (len - 1)) status = READ_CHECKSUM;
-//				byteIndex++;
-//				break;
-//			case READ_CHECKSUM:
-//				message->raw_msg[rawIndex] = buf[0];
-//				checksumCounter++;
-//				if(checksumCounter == 3)
-//				{
-//					// Check the message
-//					status = READ_PREAMBLE;
-//					// We've checked the message and it checks out
-//					// Check what message type it is
-//					message->type   = getbitu(message->raw_msg, 24 + 0, 12);
-//					message->length = rawIndex+1;
-//					// Printing useful messages from RTK1005 msg.
-//					return;
-//				}
-//				break;
-//			}
-//			rawIndex++;
-//		} else if (rdlen < 0) {
-//			printf("Error from read: %d: %s\n", rdlen, strerror(errno));
-//		}
-//		/* repeat read to get full message */
-//	} while (1);
-//}
+static uint32_t rtcm3_read(uint8_t *buff, uint32_t n, void *context __attribute__((unused)))
+{
+	int ret = read(fd, buff, n);
+	if(ret > 0)
+		return ret;
+	else
+		return 0;
+}
 
 struct LlaCoor_f lla_of_ecef_f(struct EcefCoor_f posecefpos)
 {
@@ -234,12 +136,9 @@ struct LlaCoor_f lla_of_ecef_f(struct EcefCoor_f posecefpos)
 	static const float f = 1. / 298.257223563;  /* reciprocal flattening          */
 	const float b = a * (1. - f);               /* semi-minor axis                */
 	const float b2 = b * b;
-
 	const float e2 = 2.*f - (f * f);            /* first eccentricity squared     */
 	const float ep2 = f * (2. - f) / ((1. - f) * (1. - f)); /* second eccentricity squared    */
 	const float E2 = a * a - b2;
-
-
 	const float z2 = posecefpos.z * posecefpos.z;
 	const float r2 = posecefpos.x * posecefpos.x + posecefpos.y * posecefpos.y;
 	const float r = sqrtf(r2);
@@ -256,11 +155,9 @@ struct LlaCoor_f lla_of_ecef_f(struct EcefCoor_f posecefpos)
 	const float U = sqrtf(tmp + z2);
 	const float V = sqrtf(tmp + (1 - e2) * z2);
 	const float zo = (b2 * posecefpos.z) / (a * V);
-
 	posllapos.alt = U * (1 - b2 / (a * V));
 	posllapos.lat = atanf((posecefpos.z + ep2 * zo) / r);
 	posllapos.lon = atan2f(posecefpos.y, posecefpos.x);
-
 	return posllapos;
 }
 
@@ -312,24 +209,4 @@ void set_mincount(int fd, int mcount)
 
 	if (tcsetattr(fd, TCSANOW, &tty) < 0)
 		printf("Error tcsetattr: %s\n", strerror(errno));
-}
-
-unsigned int getbitu(unsigned char *buff, int pos, int lenb)
-{
-	unsigned int bits=0;
-	int i;
-	for (i=pos;i<pos+lenb;i++) bits=(bits<<1)+((buff[i/8]>>(7-i%8))&1u);
-	return bits;
-}
-
-int getbits(unsigned char *buff, int pos, int lenb)
-{
-	unsigned int bits=getbitu(buff,pos,lenb);
-	if (lenb<=0||32<=lenb||!(bits&(1u<<(lenb-1)))) return (int)bits;
-	return (int)(bits|(~0u<<lenb)); /* extend sign */
-}
-
-static double getbits_38(unsigned char *buff, int pos)
-{
-	return (double)getbits(buff,pos,32)*64.0+getbitu(buff,pos+32,6);
 }
--- rtcm3.h
+++ rtcm3.h
@@ -7,48 +7,44 @@
  * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
  */
 
-#ifndef LIBSBP_SBP_H
-#define LIBSBP_SBP_H
+#ifndef LIBRTCM3_RTCM3_H
+#define LIBRTCM3_RTCM3_H
+#define RTCM3_PREAMBLE 0xD3
 
-#include "common.h"
 #include <errno.h>
-
-/** \addtogroup sbp
- * \{ */
+#include "common.h"
 
 /** Return value indicating success. */
-#define SBP_OK              0
-/** Return value indicating message decoded and callback executed by sbp_process. */
-#define SBP_OK_CALLBACK_EXECUTED 1
-/** Return value indicating message decoded with no associated callback in sbp_process. */
-#define SBP_OK_CALLBACK_UNDEFINED 2
+#define RTCM3_OK              0
+/** Return value indicating message decoded and callback executed by rtcm3_process. */
+#define RTCM3_OK_CALLBACK_EXECUTED 1
+/** Return value indicating message decoded with no associated callback in rtcm3_process. */
+#define RTCM3_OK_CALLBACK_UNDEFINED 2
 /** Return value indicating an error with the callback (function defined). */
-#define SBP_CALLBACK_ERROR -1
+#define RTCM3_CALLBACK_ERROR -1
 /** Return value indicating a CRC error. */
-#define SBP_CRC_ERROR      -2
-/** Return value indicating an error occured whilst sending an SBP message. */
-#define SBP_SEND_ERROR     -3
+#define RTCM3_CRC_ERROR      -2
+/** Return value indicating an error occured whilst sending an RTCM3 message. */
+#define RTCM3_SEND_ERROR     -3
 /** Return value indicating an error occured because an argument was NULL. */
-#define SBP_NULL_ERROR     -4
-
+#define RTCM3_NULL_ERROR     -4
 /** Default sender ID. Intended for messages sent from the host to the device. */
-#define SBP_SENDER_ID 0x42
+#define RTCM3_SENDER_ID 0x42
+/** RTCM3 callback function prototype definition. */
+typedef void (*rtcm3_msg_callback_t)(u16 sender_id, u8 len, u8 msg[], void *context);
 
-/** SBP callback function prototype definition. */
-typedef void (*sbp_msg_callback_t)(u16 sender_id, u8 len, u8 msg[], void *context);
-
-/** SBP callback node.
+/** RTCM3 callback node.
  * Forms a linked list of callbacks.
- * \note Must be statically allocated for use with sbp_register_callback().
+ * \note Must be statically allocated for use with rtcm3_register_callback().
  */
-typedef struct sbp_msg_callbacks_node {
+typedef struct rtcm3_msg_callbacks_node {
   u16 msg_type;                        /**< Message ID associated with callback. */
-  sbp_msg_callback_t cb;               /**< Pointer to callback function. */
+  rtcm3_msg_callback_t cb;               /**< Pointer to callback function. */
   void *context;                       /**< Pointer to a context */
-  struct sbp_msg_callbacks_node *next; /**< Pointer to next node in list. */
-} sbp_msg_callbacks_node_t;
+  struct rtcm3_msg_callbacks_node *next; /**< Pointer to next node in list. */
+} rtcm3_msg_callbacks_node_t;
 
-/** State structure for processing SBP messages. */
+/** State structure for processing RTCM3 messages. */
 typedef struct {
   enum {
 	READ_PREAMBLE = 0,
@@ -64,25 +60,340 @@ typedef struct {
   u8 n_read;
   u8 msg_buff[256]; // TODO: change to 1024?
   void* io_context;
-  sbp_msg_callbacks_node_t* sbp_msg_callbacks_head;
-} sbp_state_t;
+  rtcm3_msg_callbacks_node_t* rtcm3_msg_callbacks_head;
+} rtcm3_state_t;
 
 /** \} */
 
-s8 sbp_register_callback(sbp_state_t* s, u16 msg_type, sbp_msg_callback_t cb, void* context,
-                         sbp_msg_callbacks_node_t *node);
-void sbp_clear_callbacks(sbp_state_t* s);
-sbp_msg_callbacks_node_t* sbp_find_callback(sbp_state_t* s, u16 msg_type);
-void sbp_state_init(sbp_state_t *s);
-void sbp_state_set_io_context(sbp_state_t *s, void* context);
-s8 sbp_process(sbp_state_t *s, u32 (*read)(u8 *buff, u32 n, void* context));
-s8 sbp_send_message(sbp_state_t *s, u16 msg_type, u16 sender_id, u8 len, u8 *payload,
+s8 rtcm3_register_callback(rtcm3_state_t* s, u16 msg_type, rtcm3_msg_callback_t cb, void* context,
+                         rtcm3_msg_callbacks_node_t *node);
+void rtcm3_clear_callbacks(rtcm3_state_t* s);
+rtcm3_msg_callbacks_node_t* rtcm3_find_callback(rtcm3_state_t* s, u16 msg_type);
+void rtcm3_state_init(rtcm3_state_t *s);
+void rtcm3_state_set_io_context(rtcm3_state_t *s, void* context);
+s8 rtcm3_process(rtcm3_state_t *s, u32 (*read)(u8 *buff, u32 n, void* context));
+s8 rtcm3_send_message(rtcm3_state_t *s, u16 msg_type, u16 sender_id, u8 len, u8 *payload,
                     u32 (*write)(u8 *buff, u32 n, void* context));
+unsigned int     RTCMgetbitu(unsigned char *, int, int);
+int              RTCMgetbits(unsigned char *, int , int );
+static double    RTCMgetbits_38(unsigned char *, int );
 
 int rd_msg_len      = 0;
 int rd_msg_len1     = 0;
 int byteIndex       = 0;
 int checksumCounter = 0;
 int rawIndex        = 0;
-#endif /* LIBSBP_SBP_H */
+
+/** Register a callback for a message type.
+ * Register a callback that is called when a message
+ * with type msg_type is received.
+ *
+ * \param msg_type Message type associated with callback
+ * \param cb       Pointer to message callback function
+ * \param context  Pointer to context for callback function
+ * \param node     Statically allocated #rtcm3_msg_callbacks_node_t struct
+ * \return `RTCM3_OK` (0) if successful, `RTCM3_CALLBACK_ERROR` if callback was
+ *         already registered for that message type.
+ */
+s8 rtcm3_register_callback(rtcm3_state_t *s, u16 msg_type, rtcm3_msg_callback_t cb, void *context,
+                         rtcm3_msg_callbacks_node_t *node)
+{
+  /* Check our callback function pointer isn't NULL. */
+  if (cb == 0)
+    return RTCM3_NULL_ERROR;
+
+  /* Check our callback node pointer isn't NULL. */
+  if (node == 0)
+    return RTCM3_NULL_ERROR;
+
+  /* Check if callback was already registered for this type. */
+  if (rtcm3_find_callback(s, msg_type) != 0)
+    return RTCM3_CALLBACK_ERROR;
+
+  /* Fill in our new rtcm3_msg_callback_node_t. */
+  node->msg_type = msg_type;
+  node->cb = cb;
+  node->context = context;
+  /* The next pointer is set to NULL, i.e. this
+   * will be the new end of the linked list.
+   */
+  node->next = 0;
+
+  /* If our linked list is empty then just
+   * add the new node to the start.
+   */
+  if (s->rtcm3_msg_callbacks_head == 0) {
+    s->rtcm3_msg_callbacks_head = node;
+    return RTCM3_OK;
+  }
+
+  /* Find the tail of our linked list and
+   * add our new node to the end.
+   */
+  rtcm3_msg_callbacks_node_t *p = s->rtcm3_msg_callbacks_head;
+  while (p->next)
+    p = p->next;
+
+  p->next = node;
+
+  return RTCM3_OK;
+}
+
+/** Clear all registered callbacks.
+ * This is probably only useful for testing but who knows!
+ */
+void rtcm3_clear_callbacks(rtcm3_state_t *s)
+{
+  /* Reset the head of the callbacks list to NULL. */
+  s->rtcm3_msg_callbacks_head = 0;
+}
+
+/** Find the callback function associated with a message type.
+ * Searches through the list of registered callbacks to find the callback
+ * associated with the passed message type.
+ *
+ * \param msg_type Message type to find callback for
+ * \return Pointer to callback node (#rtcm3_msg_callbacks_node_t) or `NULL` if
+ *         callback not found for that message type.
+ */
+rtcm3_msg_callbacks_node_t* rtcm3_find_callback(rtcm3_state_t *s, u16 msg_type)
+{
+  /* If our list is empty, return NULL. */
+  if (!s->rtcm3_msg_callbacks_head)
+    return 0;
+
+  /* Traverse the linked list and return the callback
+   * function pointer if we find a node with a matching
+   * message id.
+   */
+  rtcm3_msg_callbacks_node_t *p = s->rtcm3_msg_callbacks_head;
+  do
+    if (p->msg_type == msg_type)
+      return p;
+
+  while ((p = p->next));
+
+  /* Didn't find a matching callback, return NULL. */
+  return 0;
+}
+
+/** Initialize an #rtcm3_state_t struct before use.
+ * This resets the entire state, including all callbacks.
+ * Remember to use this function to initialize the state before calling
+ * rtcm3_process() for the first time.
+ *
+ * \param s State structure
+ */
+void rtcm3_state_init(rtcm3_state_t *s)
+{
+  s->state = READ_PREAMBLE;
+
+  /* Set the IO context pointer, passed to read and write functions, to NULL. */
+  s->io_context = 0;
+
+  /* Clear the callbacks, if any, currently in s */
+  rtcm3_clear_callbacks(s);
+}
+
+
+/** Set a context to pass to all function pointer calls made by rtcm3 functions
+ * This helper function sets a void* context pointer in rtcm3_state.
+ * Whenever `rtcm3_process` calls the `read` function pointer, it passes this context.
+ * Whenever `rtcm3_send_message` calls the `write` function pointer, it passes this context.
+ * This allows C++ code to get a pointer to an object inside these functions.
+ */
+void rtcm3_state_set_io_context(rtcm3_state_t *s, void *context)
+{
+  s->io_context = context;
+}
+
+/** Read and process RTCM3 messages.
+ * Reads bytes from an input source using the provided `read` function, decodes
+ * the RTCM3 framing and performs a CRC check on the message.
+ *
+ * When an RTCM3 message is successfully received then the list of callbacks is
+ * searched for a callback corresponding to the received message type. If a
+ * callback is found then it is called with the ID of the sender, the message
+ * length and the message payload data buffer as arguments.
+ *
+ * \note rtcm3_process will always call `read` with n > 0
+ *       (aka it will attempt to always read something)
+ *
+ * The supplied `read` function must have the prototype:
+ *
+ * ~~~
+ * u32 read(u8 *buff, u32 n, void* context)
+ * ~~~
+ *
+ * where `n` is the number of bytes requested and `buff` is the buffer into
+ * which to write the received data, and `context` is the arbitrary pointer
+ * set by `rtcm3_state_set_io_context`.
+ * The function should return the number of
+ * bytes successfully written into `buff` which may be between 0 and `n`
+ * inclusive, but must never be greater than `n`.
+ *
+ * Note that `rtcm3_process` may not read all available bytes from the `read`
+ * function so the caller should loop until all bytes available from the input
+ * source have been consumed.
+ *
+ * \param s State structure
+ * \param read Function pointer to a function that reads `n` bytes from the
+ *             input source into `buff` and returns the number of bytes
+ *             successfully read.
+ * \return `RTCM3_OK` (0) if successful but no complete message yet,
+ *         `RTCM3_OK_CALLBACK_EXECUTED` (1) if message decoded and callback executed,
+ *         `RTCM3_OK_CALLBACK_UNDEFINED` (2) if message decoded with no associated
+ *         callback, and `RTCM3_CRC_ERROR` (-2) if a CRC error
+ *         has occurred. Thus can check for >0 to ensure good processing.
+ */
+s8 rtcm3_process(rtcm3_state_t *s, u32 (*read)(u8 *buff, u32 n, void *context))
+{
+	u8 buff;
+	int rdlen;
+	rdlen = (*read)(&buff, 1, s->io_context);
+	if (rdlen < 0) {
+		printf("Error from read: %d: %s\n", rdlen, strerror(errno));
+		return RTCM3_CRC_ERROR;
+	}else{
+		switch (s->state){
+		case READ_PREAMBLE:
+			s->n_read = 0;
+			if (((int) buff) == 211){ // int = 211 = 0xD3
+				s->state = READ_RESERVED;
+				rawIndex = 0;
+				s->msg_buff[s->n_read] = buff;
+				checksumCounter = 0;
+				byteIndex = 0;
+			}
+			break;
+		case READ_RESERVED:
+			s->msg_buff[s->n_read] = buff;
+			rd_msg_len1 = ((int) buff)  & 0b00000011;
+			s->state = READ_LENGTH;
+			break;
+		case READ_LENGTH:
+			s->msg_buff[s->n_read] = buff;
+			rd_msg_len = (rd_msg_len1 << 8) + ((int) buff) ;
+			s->state = READ_MESSAGE;
+			break;
+		case READ_MESSAGE:
+			s->msg_buff[s->n_read] = buff;
+			if (byteIndex == (rd_msg_len - 1)) s->state = READ_CHECKSUM;
+			byteIndex++;
+			break;
+		case READ_CHECKSUM:
+			s->msg_buff[s->n_read] = buff;
+			checksumCounter++;
+			if(checksumCounter == 3)
+			{
+				s->state = READ_PREAMBLE;
+				// Check what message type it is
+				s->msg_type  = RTCMgetbitu(s->msg_buff, 24 + 0, 12);
+				s->sender_id = RTCM3_SENDER_ID;
+				s->n_read++;
+				s->msg_len   = s->n_read;
+#ifdef NO_CALLBACK
+				return RTCM3_OK_CALLBACK_EXECUTED;
+#else
+				/* Message complete, process it. */
+				rtcm3_msg_callbacks_node_t* node = rtcm3_find_callback(s, s->msg_type);
+				if (node) {
+					(*node->cb)(s->sender_id, s->msg_len, s->msg_buff, node->context);
+					return RTCM3_OK_CALLBACK_EXECUTED;
+				} else {
+					return RTCM3_OK_CALLBACK_UNDEFINED;
+				}
+#endif
+			}
+			break;
+		}
+		s->n_read++;
+		return RTCM3_OK;
+	}
+}
+
+/** Send RTCM3 messages.
+ * Takes an RTCM3 message payload, type and sender ID then writes a message to
+ * the output stream using the supplied `write` function with the correct
+ * framing and CRC.
+ *
+ * The supplied `write` function must have the prototype:
+ *
+ * ~~~
+ * u32 write(u8 *buff, u32 n, void* context)
+ * ~~~
+ *
+ * where `n` is the number of bytes to be written and `buff` is the buffer from
+ * which to read the data to be written, and `context` is the arbitrary pointer
+ * set by `rtcm3_state_set_io_context`. The function should return the number
+ * of bytes successfully written which may be between 0 and `n`. Currently, if
+ * the number of bytes written is different from `n` then `rtcm3_send_message`
+ * will immediately return with an error.
+ *
+ * Note that `rtcm3_send_message` makes multiple calls to write and therefore if
+ * a `write` call fails then this may result in a partial message being written
+ * to the output. This should be caught by the CRC check on the receiving end
+ * but will result in lost messages.
+ *
+ * \param write Function pointer to a function that writes `n` bytes from
+ *              `buff` to the output stream  and returns the number of bytes
+ *              successfully written.
+ * \return `RTCM3_OK` (0) if successful, `RTCM3_WRITE_ERROR` if the message could
+ *         not be sent or was only partially sent.
+ */
+s8 rtcm3_send_message(rtcm3_state_t *s, u16 msg_type, u16 sender_id, u8 len, u8 *payload,
+                    u32 (*write)(u8 *buff, u32 n, void *context))
+{
+  /* Check our payload data pointer isn't NULL unless len = 0. */
+  if (len != 0 && payload == 0)
+    return RTCM3_NULL_ERROR;
+
+  /* Check our write function pointer isn't NULL. */
+  if (write == 0)
+    return RTCM3_NULL_ERROR;
+
+  u8 preamble = RTCM3_PREAMBLE;
+  if ((*write)(&preamble, 1, s->io_context) != 1)
+    return RTCM3_SEND_ERROR;
+
+  if ((*write)((u8*)&msg_type, 2, s->io_context) != 2)
+    return RTCM3_SEND_ERROR;
+
+  if ((*write)((u8*)&sender_id, 2, s->io_context) != 2)
+    return RTCM3_SEND_ERROR;
+
+  if ((*write)(&len, 1, s->io_context) != 1)
+    return RTCM3_SEND_ERROR;
+
+  if (len > 0) {
+    if ((*write)(payload, len, s->io_context) != len)
+      return RTCM3_SEND_ERROR;
+  }
+  return RTCM3_OK;
+}
+
+/** \} */
+/** \} */
+
+unsigned int RTCMgetbitu(unsigned char *buff, int pos, int lenb)
+{
+	unsigned int bits=0;
+	int i;
+	for (i=pos;i<pos+lenb;i++) bits=(bits<<1)+((buff[i/8]>>(7-i%8))&1u);
+	return bits;
+}
+
+int RTCMgetbits(unsigned char *buff, int pos, int lenb)
+{
+	unsigned int bits=RTCMgetbitu(buff,pos,lenb);
+	if (lenb<=0||32<=lenb||!(bits&(1u<<(lenb-1)))) return (int)bits;
+	return (int)(bits|(~0u<<lenb)); /* extend sign */
+}
+
+static double RTCMgetbits_38(unsigned char *buff, int pos)
+{
+	return (double)RTCMgetbits(buff,pos,32)*64.0+RTCMgetbitu(buff,pos+32,6);
+}
+
+#endif /* LIBRTCM3_RTCM3_H */
 
